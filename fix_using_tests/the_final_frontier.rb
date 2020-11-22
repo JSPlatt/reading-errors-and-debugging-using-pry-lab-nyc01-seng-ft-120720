c92540b8a75a0002ab776303369cf6764973308a
Jeffrey Platt <jsplatt@gmail.com>
2020-11-22 21:24:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,10 +7,11 @@ end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{generate_star_date}."
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
+  engage
 end
